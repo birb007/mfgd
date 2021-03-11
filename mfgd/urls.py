58c9f400da2ff637c68af97a07b3bf01527b1638
Paul Bekaert <2517073B@student.gla.ac.uk>
2021-03-11 13:55:48 +0000

    Added register

--- mfgd/urls.py
+++ mfgd/urls.py
@@ -21,14 +21,12 @@ from django.conf.urls import handler404
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    re_path(
-        r"(?P<repo_name>\w+)/view/(?P<oid>\w+)/(?P<path>\S*)", views.view, name="view"
-    ),
-    re_path(r"(?P<repo_name>\w+)/info/(?P<oid>\w+)/?", views.info, name="info"),
+    path("", views.index, name='index'),
+    re_path(r"view/(?P<oid>\w+)/(?P<path>\S*)", views.view, name="view"),
     path("admin/", admin.site.urls),
-    path("logout/", views.user_logout, name="logout"),
-    path("login/", views.user_login, name="login"),
+    path('logout/', views.user_logout, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
 ]
 
 handler404 = views.error_404
